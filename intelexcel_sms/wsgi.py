7496d6370dea28503595f0b56bb835b0c7f02031
masood azhar <masoodazhar60@gmail.com>
2022-02-08 20:51:18 +0500

    tests live status

--- intelexcel_sms/wsgi.py
+++ intelexcel_sms/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for intelexcel_sms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see 
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
